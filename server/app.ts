1e286e41e8ebc2f229efc1a7aa008e4e825d7706
Eugene Goldberg <e602684@CHIMAC11592-2.local>
2019-09-17 16:32:39 -0500

    second

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,15 @@ import * as morgan from 'morgan';
 import * as Knex from 'knex';
 import { Model } from 'objection';
 
+const elasticsearch = require('elasticsearch');
+
+const client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
+
+
 /** Redis */
 // import { Init } from './db/redis';
 // Init();
@@ -29,6 +38,8 @@ Model.knex(knex);
 const _clientDir = '../angular-express';
 
 export const app: express.Application = express();
+
+
 
 app
   .use(bodyParser.urlencoded({ extended: false }))
@@ -60,13 +71,36 @@ app.use('/', express.static(path.resolve(__dirname, `${_clientDir}/`)));
  * Spa Res Sender
  */
 const renderIndex = (req: express.Request, res: express.Response) => {
+
+  client.ping({
+    // ping usually has a 3000ms timeout
+    requestTimeout: 1000
+// tslint:disable-next-line:no-any
+  }, (error: any) => {
+    if (error) {
+      console.trace('elasticsearch cluster is down!');
+    } else {
+      console.log('All is well');
+    }
+  });
+
   res.sendFile(path.resolve(__dirname, `${_clientDir}/index.html`));
+};
+
+const message = 'hello';
+
+const hello = (req: express.Request, res: express.Response) => {
+  res.json(message);
 };
 
 /**
  * Prevent server routing and use @ng2-router.
  */
 app.get('/*', renderIndex);
+
+setTimeout(() => {
+  console.log('timeout completed');
+}, 1000);
 
 /**
  * Server with gzip compression.
